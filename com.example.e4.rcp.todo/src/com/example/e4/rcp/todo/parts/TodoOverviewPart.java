2f0031469c3d89a352541ce461f5f7a6cd5ee6ac
maiconmichelon <maicon.fermo@gmail.com>
2016-07-24 01:24:34 -0300

    adicionando menu

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoOverviewPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoOverviewPart.java
@@ -1,5 +1,15 @@
 package com.example.e4.rcp.todo.parts;
 
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
 public class TodoOverviewPart {
 
+	@PostConstruct
+	public void createControls(Composite parent) {
+	  System.out.println(this.getClass().getSimpleName()
+	  + " @PostConstruct method called.");
+	}
+	
 }
